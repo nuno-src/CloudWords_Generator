69c6aa534c2b57c637295b2941b11d92d51121c3
nuno <75589883+nunocardoso7@users.noreply.github.com>
2022-04-30 15:37:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from wordcloud import WordCloud, STOPWORDS,ImageColorGenerator
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import numpy as np
 import matplotlib.pyplot as plt
 import PIL.Image
